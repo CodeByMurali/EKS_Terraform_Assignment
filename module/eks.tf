c4df0c0069b8c0810b09e9bf2766b6b3c5f08680
murali.rajendran <murali.rajendran@veeva.com>
2025-06-05 13:29:23 -0400

    VPC module params changed

--- module/eks.tf
+++ module/eks.tf
@@ -1,14 +1,11 @@
 resource "aws_eks_cluster" "eks" {
 
   # conditionally creates the resource based on whether var.is-eks-cluster-enabled is true. If true, one instance is created; otherwise, none.
-  count = var.is-eks-cluster-enabled == true ? 1 : 0
-  name  = var.cluster-name
-  # This role is used by the EKS control plane to create and manage AWS resources by assuming the specified role.
+  count    = var.is-eks-cluster-enabled == true ? 1 : 0
+  name     = var.cluster-name
   role_arn = aws_iam_role.eks-cluster-role[count.index].arn
   version  = var.cluster-version
 
-  # The vpc_config block is a nested block that configures the VPC settings for the EKS cluster.
-  # It is available by default in the aws_eks_cluster resource provided by the AWS provider.
   vpc_config {
 
     # By specifying these ptivate subnet IDs, the EKS cluster control plane resources will be deployed within these private subnets
@@ -18,24 +15,13 @@ resource "aws_eks_cluster" "eks" {
       aws_subnet.private-subnet[2].id
     ]
 
-    # This is set to true to allow the EKS cluster to communicate with private IP addresses in the VPC.
-    # We will be using a bastion host to access the EKS cluster that is deployed in the private subnets.
     endpoint_private_access = var.endpoint-private-access
-
-    # This is set to false to prevent the EKS cluster from communicating with the public internet.
-    # if set to true, the EKS cluster will have a public endpoint that can be accessed from the internet.
-    endpoint_public_access = var.endpoint-public-access
-
-    # By default used the main security group of the VPC which allows all traffic.
-    # But we are using a custom security group for the EKS cluster
-    security_group_ids = [aws_security_group.eks-cluster-sg.id]
+    endpoint_public_access  = var.endpoint-public-access
+    security_group_ids      = [aws_security_group.eks-cluster-sg.id]
   }
 
   # authentication_mode: Specifies the mode of authentication. 
   #   - "CONFIG_MAP": Uses a Kubernetes ConfigMap for authentication.
-  #   - "IAM": Uses AWS IAM for authentication.
-  # bootstrap_cluster_creator_admin_permissions: 
-  # Grants admin permissions to the user who created the cluster.
   access_config {
     authentication_mode                         = "CONFIG_MAP"
     bootstrap_cluster_creator_admin_permissions = true
@@ -43,8 +29,6 @@ resource "aws_eks_cluster" "eks" {
 
   # Needed to explicity define the depends_on argument to ensure that the EKS cluster is created 
   # after the IAM role policy attachment for the EKS cluster policy is created.
-
-  # If not declared, EKS will not be able to properly delete EKS managed EC2 infrastructure.
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
   ]
@@ -57,11 +41,9 @@ resource "aws_eks_cluster" "eks" {
 
 # TLS certificates ensure that the communication between the Kubernetes control plane and the worker nodes is secure.
 resource "aws_iam_openid_connect_provider" "eks-oidc" {
-  client_id_list = ["sts.amazonaws.com"]
-  // Obtain the SHA-1 fingerprint (thumbprint) of the TLS certificate for the EKS cluster.
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
 
-  // Obtain the URL of the EKS cluster's API server.
   url = data.tls_certificate.eks-certificate.url
 }
 
--- module/vpc.tf
+++ module/vpc.tf
@@ -1,22 +1,13 @@
-# Define a local value 'cluster-name' as a shorthand reference to 'var.cluster-name' 
-# for improved readability and maintainability.
-# Define local values for the Terraform configuration.
-# The 'cluster-name' local value is set to the value of the 'cluster-name' variable.
 locals {
   cluster-name = var.cluster-name
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block       = var.cidr-block
-  instance_tenancy = "default"
-  # Required for EKS. This will enable DNS resolution for the VPC.
+  cidr_block           = var.cidr-block
+  instance_tenancy     = "default"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
-  # Required for EKS. This will enable DNS support for the VPC.
-  enable_dns_support = true
-
-  # Tagging kubernetes.io/cluster is usually for resources like subnets and IGWs to associate them with a Kubernetes cluster.
-  # The VPC itself doesn't need this tag as it is a broader network container.
   tags = {
     Name = var.vpc-name
     Env  = var.env
@@ -31,14 +22,7 @@ resource "aws_internet_gateway" "igw" {
     Name = var.igw-name
     env  = var.env
 
-    # Tag to associate the VPC with the Kubernetes cluster.
-    # This tag is required for the Kubernetes cluster to recognize and manage resources within the VPC.
-    # The value "owned" indicates that the cluster owns the VPC.
-    # This tag is used to associate AWS resources with a specific Kubernetes cluster.
-    # The "kubernetes.io/cluster/${local.cluster-name}" tag is typically used by Kubernetes
-    # to identify resources that belong to a particular cluster. The value "owned" indicates
-    # that the resource is owned by the cluster. This tag is not mandatory for an internet gateway,
-    # but it is useful for resource management and organization within a Kubernetes cluster.
+
     "kubernetes.io/cluster/${local.cluster-name}" = "owned"
   }
 
@@ -46,56 +30,34 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public-subnet" {
-  # This will iterate over the number of public subnets defined in the variable `var.pub-subnet-count`.
-  count  = var.pub-subnet-count
-  vpc_id = aws_vpc.vpc.id
-
-  # Assigns a CIDR block to the VPC from the list of public CIDR blocks defined 
-  # in the variable `var.pub-cidr-block`.
-  # The specific CIDR block is selected based on the current index of the resource being created.
-  cidr_block        = element(var.pub-cidr-block, count.index)
-  availability_zone = element(var.pub-availability-zone, count.index)
-
-  #Required for EKS. If Instance (worker node) launched in public subnet will have public IP.
-  #We will also use this subnet for ELB.
+  count                   = var.pub-subnet-count
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.pub-cidr-block, count.index)
+  availability_zone       = element(var.pub-availability-zone, count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.pub-sub-name}-${count.index + 1}"
-    Env  = var.env
-
-    # This tag is used to associate the VPC with the EKS cluster.
-    # The value "owned" indicates that the VPC is exclusively owned by the EKS cluster.
-    # If the value were "shared", it would indicate that the VPC is shared among multiple clusters or resources.
-    # Note: When deleting the EKS cluster, resources with the "owned" tag will be deleted automatically.
-    # Resources with the "shared" tag will not be deleted automatically.
-    # Mandatory for EKS. This tag is mandatory for associating the subnet with the EKS cluster.
+    Name                                          = "${var.pub-sub-name}-${count.index + 1}"
+    Env                                           = var.env
     "kubernetes.io/cluster/${local.cluster-name}" = "owned"
-
-    # Mandatory for EKS. This tag will allow the public subnet to be used by the ELB.
-    # when loadbalncer service is created, EKS will look for this tag to identify the subnet.
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 
   depends_on = [aws_vpc.vpc, ]
 }
 
 resource "aws_subnet" "private-subnet" {
-  count             = var.pri-subnet-count
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = element(var.pri-cidr-block, count.index)
-  availability_zone = element(var.pri-availability-zone, count.index)
-  # we donot need public IP for worker nodes.
+  count                   = var.pri-subnet-count
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.pri-cidr-block, count.index)
+  availability_zone       = element(var.pri-availability-zone, count.index)
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.pri-sub-name}-${count.index + 1}"
-    Env  = var.env
-    # This tag means that this subnet would be used only by the specific EKS cluster for internal ELB.
-    # If shared is provided, it means that this subnet can be used by multiple EKS clusters.
+    Name                                          = "${var.pri-sub-name}-${count.index + 1}"
+    Env                                           = var.env
     "kubernetes.io/cluster/${local.cluster-name}" = "owned"
-    # note the use of "internal-elb" instead of "elb" for private subnets
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 
   depends_on = [aws_vpc.vpc]
@@ -130,29 +92,18 @@ resource "aws_route_table_association" "name" {
 }
 // NAT Gateway needs elastic IP to be associated with it so that  it can be accessed from internet.
 resource "aws_eip" "ngw-eip" {
-  # The 'domain' attribute specifies the scope in which the Elastic IP (EIP) will be used.
-  # Possible values are "vpc" for VPC-specific EIPs and "standard" for standard EIPs.
-  # In this case, "vpc" indicates that the EIP will be associated with a VPC resource.
-  # standard indicates that the EIP will be associated with an EC2 instance.
   domain = "vpc"
 
   tags = {
     Name = var.eip-name
   }
-
-  # Elastic IP (EIP) can only be created after the VPC is created. 
-  # This is because an EIP needs to be associated with a network interface, 
-  # which in turn is associated with a subnet within a VPC. 
   depends_on = [aws_vpc.vpc]
 
 }
 
 resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.ngw-eip.id
-  # we are allowing only one subnet to use this NAT Gateway.
-  # which means the oher private subnet will also use this NAT Gateway to access internet.
-  # This is made possible by route table rules.
-  subnet_id = aws_subnet.public-subnet[0].id
+  subnet_id     = aws_subnet.public-subnet[0].id
 
   tags = {
     Name = var.ngw-name
@@ -164,7 +115,6 @@ resource "aws_nat_gateway" "ngw" {
 }
 
 # The private route table internet traffic is associated with the NAT Gateway to allow the private subnet to access the internet.
-# Note: The traffic within the VPC is inherited by default by the default/main route rule which needs no additional configuration.
 resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.vpc.id
 
