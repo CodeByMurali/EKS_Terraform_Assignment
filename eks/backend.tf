19b2422924d0680832879ae51016fb4a09622597
murali.rajendran <murali.rajendran@veeva.com>
2025-06-05 06:51:38 -0400

    Configure terraform backend

---
+++ eks/backend.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "~> 1.12.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "use1-remote-terraform-state-file-bucket-murali"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "Lock-Files"
+  }
+}
+
+provider "aws" {
+  region = var.aws-region
+}
